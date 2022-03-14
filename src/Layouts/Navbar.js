790b91bd739937389b1099146633b012a4651e59
Agusfb <86432758+Agusfb@users.noreply.github.com>
2022-03-13 23:44:17 -0300

    Post afterclass

---
+++ src/Layouts/Navbar.js
@@ -0,0 +1,24 @@
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+
+function NavBarr() {
+    return(
+        
+      <Navbar bg="light"  expand="lg">
+        <Container>
+         <Navbar.Brand href="#home">Tienda React</Navbar.Brand>
+         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+         <Navbar.Collapse id="basic-navbar-nav">
+         <Nav className="me-auto">
+           <Nav.Link href="#home">Inicio</Nav.Link>
+           <Nav.Link href="#link">Contacto</Nav.Link>
+           
+         </Nav>
+         </Navbar.Collapse>
+        </Container>
+      </Navbar>
+        
+    )
+}
+export default NavBarr
